4a9604d5d059b2699699c2213903801bf901ba63
Paul Quenault <plquenault@gmail.com>
2024-02-09 01:16:17 +0100

    Update renovate-config.js

--- .github/renovate-config.js
+++ .github/renovate-config.js
@@ -1,5 +1,6 @@
 module.exports = {
   branchPrefix: 'renovate-bot/',
+  repositories: ['demo-renovate'],
   // gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
   // onboarding: false,
   platform: 'github',
